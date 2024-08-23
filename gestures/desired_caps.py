b659611fdcf47844b7ba3f9e6d59ac2a6ef61c78
Raed Eleyan <raedmalyan@gmail.com>
2024-08-23 11:28:57 +0300

    updated desired_caps file to contain contacts desired capability

--- gestures/desired_caps.py
+++ gestures/desired_caps.py
@@ -6,3 +6,11 @@ apidemos = {
     "appPackage": "io.appium.android.apis",
     "appActivity": ".ApiDemos",
 }
+
+contacts = {
+    "appPackage": "com.android.contacts",
+    "appActivity": ".activities.PeopleActivity",
+    "platformName": "Android",
+    "deviceName": "Pixel 8 Pro API 26",
+    "udid": "emulator-5554"
+}
